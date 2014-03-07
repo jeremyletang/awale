9cbec653ea63d63fdcb59344ba480fac7f527690
JeremyLetang <letang.jeremy@gmail.com>
2014-03-07 18:08:23 -0500

    ajout current_player + check du tour + check du range

--- awale.pl
+++ awale.pl
@@ -2,10 +2,12 @@
 
 :- dynamic(state/2).
 :- dynamic(score/2).
+:- dynamic(current_player/1).
 state(human, [4,4,4,4,4,4]).
 state(ia, [4,4,4,4,4,4]).
 score(human, 0).
 score(ia, 0).
+current_player(human).
 
     % States / Score utils
 
@@ -17,6 +19,14 @@ set_score(Player, NewScore) :-
     retract(score(Player, _)),
     assert(score(Player, NewScore)).
 
+set_player:-
+    current_player(Player),
+    Player == human ->
+    retract(current_player(_)),
+    assert(current_player(ia))
+    ; retract(current_player(_)),
+    assert(current_plauer(human)).
+
     % List utils
 
 set(Lin, Index, Value, Lout) :-
@@ -27,10 +37,34 @@ set(Lin, Index, Value, Lout) :-
 get(Lin, Index, Value) :-
     nth0(Index, Lin, Value).
 
+    % Player play
+
+jouer(Position) :-
+    Position > 6 ->
+    writeln('Impossible de selectionner une position superieur a la case 6')
+    ; Position < 1 ->
+    writeln('Impossible de selectionner une position inferieur a la case 1')
+    ; current_player(Player),
+    Player == human ->
+    writeln('human play'),
+    set_player,
+    draw_game
+    ; ansi_format([fg(red)], 'C\'est au tour de l\'IA de jouer maintenant !', []).
+
+    % IA play
+
+jouer_ia:-
+    current_player(Player),
+    Player == ia ->
+    writeln('ia play'),
+    set_player,
+    draw_game
+    ;  ansi_format([fg(red)], 'C\'est au tour du joueur de jouer maintenant !', []).
+
     % Drawing
 
 draw_values(List, X) :-
-X < 6 ->
+    X < 6 ->
     get(List, X, Value),
     write(Value),
     write('  |  '),
@@ -39,20 +73,28 @@ X < 6 ->
     ; write('          |      ').
 
 draw_game:-
-    write('\n'),
-    write('----------------- PLATEAU DE JEU ---------------            ---- SCORE ----\n'),
-    write('------------------------------------------------            ---------------\n'),
-    write('|  IA      |  '),
-    state(ia, StateIA),
-    draw_values(StateIA, 0),
-    score(ia, ScoreIA),
-    write(ScoreIA),
-    write('     |\n'),
-    write('------------------------------------------------            ---------------\n'),
-    write('|  Humain  |  '),
     state(human, StateHuman),
-    draw_values(StateHuman, 0),
+    state(ia, StateIA),
     score(human, ScoreHuman),
+    score(ia, ScoreIA),
+    write('\n'),
+    write('----------------- PLATEAU DE JEU ---------------            \
+---- SCORE ----\n'),
+    write('------------------------------------------------            \
+---------------\n'),
+    write('|  IA      |  '),
+    draw_values(StateIA, 0),
+    write(ScoreIA),
+    write('      |\n'),
+    write('------------------------------------------------            \
+---------------\n'),
+    write('|  Humain  |  '),
+    draw_values(StateHuman, 0),
     write(ScoreHuman),
-    write('     |\n'),
-    write('------------------------------------------------            ---------------\n\n').
+    write('      |\n'),
+    write('------------------------------------------------            \
+---------------\n\n').
+
+% Draw the game
+
+%:- draw_game.
