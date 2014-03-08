92e3c8b9cae79080b745c437e4d73a471477dedb
JeremyLetang <letang.jeremy@gmail.com>
2014-03-07 20:52:27 -0500

    dispatch des graines dans les trous suivants.

--- awale.pl
+++ awale.pl
@@ -1,19 +1,29 @@
-   % Init some stuff
+% Init some stuff
 
 :- dynamic(state/2).
 :- dynamic(score/2).
 :- dynamic(current_player/1).
-state(human, [0,4,4,4,4,4]).
+state(human, [4,4,4,4,4,4]).
 state(ia, [4,4,4,4,4,4]).
 score(human, 0).
 score(ia, 0).
 current_player(human).
 
-    % States / Score utils
+% States / Score utils
 
 set_state(Player, NewState) :-
     retract(state(Player, _)),
     assert(state(Player, NewState)).
+
+copy_state(Player, A) :-
+    state(Player, T),
+    get(T, 0, V1),
+    get(T, 1, V2),
+    get(T, 2, V3),
+    get(T, 3, V4),
+    get(T, 4, V5),
+    get(T, 5, V6),
+    A = [V1, V2, V3, V4, V5, V6].
 
 set_score(Player, NewScore) :-
     retract(score(Player, _)),
@@ -27,7 +37,7 @@ set_player:-
     ; retract(current_player(_)),
     assert(current_player(human)).
 
-    % List utils
+% List utils
 
 set(Lin, Index, Value, Lout) :-
     length(Ltmp, Index),
@@ -37,7 +47,13 @@ set(Lin, Index, Value, Lout) :-
 get(Lin, Index, Value) :-
     nth0(Index, Lin, Value).
 
-    % Player play
+copy(List1, List2) :-
+    inner_copy(List1,List2).
+
+inner_copy([],[]).
+inner_copy([Head|Next1],[Head|Next2]) :- inner_copy(Next1,Next2).
+
+% Player play
 
 test_slot_empty(Slot) :-
     state(human, State),
@@ -47,8 +63,49 @@ test_slot_empty(Slot) :-
     ; ansi_format([fg(red)], 'Vous ne pouver jouer une case vide!', []),
     fail.
 
-apply_change(Slot) :-
+dispatch_ia(Slot, Seeds, Player) :-
+    Seeds > 0 ->
+    (
+     Slot @>= 0 ->
+     copy_state(ia, State),
+     get(State, Slot, SlotSeeds),
+     NewSlotSeeds is SlotSeeds + 1,
+     set(State, Slot, NewSlotSeeds, NewState),
+     set_state(ia, NewState),
+     NewSeeds is Seeds - 1,
+     NewSlot is Slot - 1,
+     dispatch_ia(NewSlot, NewSeeds, Player)
+     ; NewSeeds is Seeds - 1,
+     dispatch_player(0, NewSeeds, Player)
+    )
+    ; true.
+
+
+dispatch_player(Slot, Seeds, Player) :-
+    Seeds > 0 ->
+    (
+     Slot @=< 5 ->
+     copy_state(human, State),
+     get(State, Slot, SlotSeeds),
+     NewSlotSeeds is SlotSeeds + 1,
+     set(State, Slot, NewSlotSeeds, NewState),
+     set_state(human, NewState),
+     NewSeeds is Seeds - 1,
+     NewSlot is Slot + 1,
+     dispatch_player(NewSlot, NewSeeds, Player)
+     ; NewSeeds is Seeds - 1,
+     dispatch_ia(5, NewSeeds, Player)
+    )
+    ; true.
+
+apply_change_player(Slot) :-
     test_slot_empty(Slot),
+    copy_state(human, A),
+    get(A, Slot, Seeds),
+    set(A, Slot, 0, B),
+    set_state(human, B),
+    NewSlot is Slot + 1,
+    dispatch_player(NewSlot, Seeds, human),
     set_player,
     draw_game.
 
@@ -60,11 +117,11 @@ jouer(Position) :-
     ; current_player(Player),
     Player == human ->
     Slot is Position - 1,
-    apply_change(Slot)
+    apply_change_player(Slot)
     ; ansi_format([fg(red)], 'C\'est au tour de l\'IA de jouer maintenant !',
           []).
 
-    % IA play
+% IA play
 
 jouer_ia:-
     current_player(Player),
@@ -75,7 +132,7 @@ jouer_ia:-
     ;  ansi_format([fg(red)], 'C\'est au tour du joueur de jouer maintenant !',
            []).
 
-    % Drawing
+% Drawing
 
 draw_values(List, X) :-
     X < 6 ->
